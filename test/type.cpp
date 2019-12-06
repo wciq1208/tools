9ae393e38d5bd47bc8591cda178798fb27ad7213
Your Name <you@example.com>
2019-12-06 20:54:08 +0800

    namespace

--- test/type.cpp
+++ test/type.cpp
@@ -7,75 +7,74 @@
 #include <iostream>
 
 using namespace type;
-using namespace type::list;
 
 void list_replace_by_type() {
-    using list = Creator<int, float, double, int, float >::type;
-    std::cout << std::is_same<ReplaceByType<list, int , char>::type, Creator<char, float, double, char, float >::type>::value << std::endl;
-    std::cout << std::is_same<ReplaceByType<list, char , int>::type, list>::value << std::endl;
-    std::cout << std::is_same<ReplaceByType<list, float , int>::type, Creator<int, int, double, int, int >::type>::value << std::endl;
+    using list1 = list::Creator<int, float, double, int, float >::type;
+    std::cout << std::is_same<list::ReplaceByType<list1, int , char>::type, list::Creator<char, float, double, char, float >::type>::value << std::endl;
+    std::cout << std::is_same<list::ReplaceByType<list1, char , int>::type, list1>::value << std::endl;
+    std::cout << std::is_same<list::ReplaceByType<list1, float , int>::type, list::Creator<int, int, double, int, int >::type>::value << std::endl;
 }
 
 void list_replace_by_index() {
-    using list = Creator<int, float, double, int, float >::type;
-    std::cout << std::is_same<ReplaceByIndex<list, 0, char >::type, Creator<char, float, double, int, float >::type>::value << std::endl;
-    std::cout << std::is_same<ReplaceByIndex<list, 0, int >::type, list>::value << std::endl;
-    std::cout << std::is_same<ReplaceByIndex<list, 1, char >::type, Creator<int, char, double, int, float >::type>::value << std::endl;
-    std::cout << std::is_same<ReplaceByIndex<list, 4, char >::type, Creator<int, float, double, int, char >::type>::value << std::endl;
-    std::cout << std::is_same<ReplaceByIndex<list, 10, char >::type, list>::value << std::endl;
+    using list1 = list::Creator<int, float, double, int, float >::type;
+    std::cout << std::is_same<list::ReplaceByIndex<list1, 0, char >::type, list::Creator<char, float, double, int, float >::type>::value << std::endl;
+    std::cout << std::is_same<list::ReplaceByIndex<list1, 0, int >::type, list1>::value << std::endl;
+    std::cout << std::is_same<list::ReplaceByIndex<list1, 1, char >::type, list::Creator<int, char, double, int, float >::type>::value << std::endl;
+    std::cout << std::is_same<list::ReplaceByIndex<list1, 4, char >::type, list::Creator<int, float, double, int, char >::type>::value << std::endl;
+    std::cout << std::is_same<list::ReplaceByIndex<list1, 10, char >::type, list1>::value << std::endl;
 }
 
 void list_index_of() {
-    using list = Creator<int, float, double, int, float >::type;
-    std::cout << std::is_same<IndexOf<list, 1>::type, float>::value << std::endl;
-    std::cout << std::is_same<IndexOf<list, 0>::type, int>::value << std::endl;
-    std::cout << std::is_same<IndexOf<list, 4>::type, float>::value << std::endl;
+    using list1 = list::Creator<int, float, double, int, float >::type;
+    std::cout << std::is_same<list::IndexOf<list1, 1>::type, float>::value << std::endl;
+    std::cout << std::is_same<list::IndexOf<list1, 0>::type, int>::value << std::endl;
+    std::cout << std::is_same<list::IndexOf<list1, 4>::type, float>::value << std::endl;
 }
 
 void list_find() {
-    using list = Creator<int, float, double, int, char >::type;
-    std::cout << (Find<list, float>::value == 1) << std::endl;
-    std::cout << (Find<list, int>::value == 0) << std::endl;
-    std::cout << (Find<list, char>::value == 4) << std::endl;
-    std::cout << (Find<list, bool >::value == -1) << std::endl;
+    using list1 = list::Creator<int, float, double, int, char >::type;
+    std::cout << (list::Find<list1, float>::value == 1) << std::endl;
+    std::cout << (list::Find<list1, int>::value == 0) << std::endl;
+    std::cout << (list::Find<list1, char>::value == 4) << std::endl;
+    std::cout << (list::Find<list1, bool >::value == -1) << std::endl;
 }
 
 void list_length() {
-    using list1 = Creator<int, float, double, int, char >::type;
-    using list2 = Creator<int>::type;
-    std::cout << (Length<list1>::value == 5) << std::endl;
-    std::cout << (Length<list2>::value == 1) << std::endl;
+    using list1 = list::Creator<int, float, double, int, char >::type;
+    using list2 = list::Creator<int>::type;
+    std::cout << (list::Length<list1>::value == 5) << std::endl;
+    std::cout << (list::Length<list2>::value == 1) << std::endl;
 }
 
 void list_remove_by_type() {
-    using list1 = Creator<int, float, double, int, char >::type;
-    using list2 = Creator<int, float, double, int, float >::type;
-    using list3 = Creator<int, float, double, int, float >::type;
-    std::cout << std::is_same<RemoveByType<list1, double >::type, Creator<int, float, int, char >::type>::value << std::endl;
-    std::cout << std::is_same<RemoveByType<list2, char >::type, list2>::value << std::endl;
-    std::cout << std::is_same<RemoveByType<list3, int >::type, Creator<float, double, float >::type>::value << std::endl;
+    using list1 = list::Creator<int, float, double, int, char >::type;
+    using list2 = list::Creator<int, float, double, int, float >::type;
+    using list3 = list::Creator<int, float, double, int, float >::type;
+    std::cout << std::is_same<list::RemoveByType<list1, double >::type, list::Creator<int, float, int, char >::type>::value << std::endl;
+    std::cout << std::is_same<list::RemoveByType<list2, char >::type, list2>::value << std::endl;
+    std::cout << std::is_same<list::RemoveByType<list3, int >::type, list::Creator<float, double, float >::type>::value << std::endl;
 }
 
 void list_remove_by_index() {
-    using list = Creator<int, float, double, int, char >::type;
-    std::cout << std::is_same<RemoveByIndex<list, 0 >::type, Creator<float, double, int, char >::type>::value << std::endl;
-    std::cout << std::is_same<RemoveByIndex<list, 1 >::type, Creator<int, double, int, char >::type>::value << std::endl;
-    std::cout << std::is_same<RemoveByIndex<list, 4 >::type, Creator<int, float, double, int >::type>::value << std::endl;
-    std::cout << std::is_same<RemoveByIndex<list, 10 >::type, list>::value << std::endl;
+    using list1 = list::Creator<int, float, double, int, char >::type;
+    std::cout << std::is_same<list::RemoveByIndex<list1, 0 >::type, list::Creator<float, double, int, char >::type>::value << std::endl;
+    std::cout << std::is_same<list::RemoveByIndex<list1, 1 >::type, list::Creator<int, double, int, char >::type>::value << std::endl;
+    std::cout << std::is_same<list::RemoveByIndex<list1, 4 >::type, list::Creator<int, float, double, int >::type>::value << std::endl;
+    std::cout << std::is_same<list::RemoveByIndex<list1, 10 >::type, list1>::value << std::endl;
 }
 
 void list_join() {
-    using list1 = Creator<int, float, double, int, char >::type;
-    using list2 = Creator<int, float, double, int, char >::type;
-    std::cout << std::is_same<Join<list1, list2 >::type, Creator<int, float, double, int, char, int, float, double, int, char>::type>::value << std::endl;
+    using list1 = list::Creator<int, float, double, int, char >::type;
+    using list2 = list::Creator<int, float, double, int, char >::type;
+    std::cout << std::is_same<list::Join<list1, list2 >::type, list::Creator<int, float, double, int, char, int, float, double, int, char>::type>::value << std::endl;
 }
 
 void list_insert() {
-    using list1 = Creator<int, float, double, int, char >::type;
-    std::cout << std::is_same<Insert<list1, 0, int >::type, Creator<int, int, float, double, int, char>::type>::value << std::endl;
-    std::cout << std::is_same<Insert<list1, 1, char >::type, Creator<int, char, float, double, int, char>::type>::value << std::endl;
-    std::cout << std::is_same<Insert<list1, 5, int >::type, Creator<int, float, double, int, char, int>::type>::value << std::endl;
-    std::cout << std::is_same<Insert<list1, 10, int >::type, Creator<int, float, double, int, char, int>::type>::value << std::endl;
+    using list1 = list::Creator<int, float, double, int, char >::type;
+    std::cout << std::is_same<list::Insert<list1, 0, int >::type, list::Creator<int, int, float, double, int, char>::type>::value << std::endl;
+    std::cout << std::is_same<list::Insert<list1, 1, char >::type, list::Creator<int, char, float, double, int, char>::type>::value << std::endl;
+    std::cout << std::is_same<list::Insert<list1, 5, int >::type, list::Creator<int, float, double, int, char, int>::type>::value << std::endl;
+    std::cout << std::is_same<list::Insert<list1, 10, int >::type, list::Creator<int, float, double, int, char, int>::type>::value << std::endl;
 }
 
 
