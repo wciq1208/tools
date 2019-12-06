49eda674179ce6e407ac53192a43b52a8da6f805
Your Name <you@example.com>
2019-12-06 20:06:03 +0800

    map

--- test/type.cpp
+++ test/type.cpp
@@ -3,18 +3,20 @@
 //
 
 #include "list.h"
+#include "map.h"
 #include <iostream>
 
+using namespace type;
 using namespace type::list;
 
-void replace_by_type() {
+void list_replace_by_type() {
     using list = Creator<int, float, double, int, float >::type;
     std::cout << std::is_same<ReplaceByType<list, int , char>::type, Creator<char, float, double, char, float >::type>::value << std::endl;
     std::cout << std::is_same<ReplaceByType<list, char , int>::type, list>::value << std::endl;
     std::cout << std::is_same<ReplaceByType<list, float , int>::type, Creator<int, int, double, int, int >::type>::value << std::endl;
 }
 
-void replace_by_index() {
+void list_replace_by_index() {
     using list = Creator<int, float, double, int, float >::type;
     std::cout << std::is_same<ReplaceByIndex<list, 0, char >::type, Creator<char, float, double, int, float >::type>::value << std::endl;
     std::cout << std::is_same<ReplaceByIndex<list, 0, int >::type, list>::value << std::endl;
@@ -23,14 +25,14 @@ void replace_by_index() {
     std::cout << std::is_same<ReplaceByIndex<list, 10, char >::type, list>::value << std::endl;
 }
 
-void index_of() {
+void list_index_of() {
     using list = Creator<int, float, double, int, float >::type;
     std::cout << std::is_same<IndexOf<list, 1>::type, float>::value << std::endl;
     std::cout << std::is_same<IndexOf<list, 0>::type, int>::value << std::endl;
     std::cout << std::is_same<IndexOf<list, 4>::type, float>::value << std::endl;
 }
 
-void find() {
+void list_find() {
     using list = Creator<int, float, double, int, char >::type;
     std::cout << (Find<list, float>::value == 1) << std::endl;
     std::cout << (Find<list, int>::value == 0) << std::endl;
@@ -38,14 +40,14 @@ void find() {
     std::cout << (Find<list, bool >::value == -1) << std::endl;
 }
 
-void length() {
+void list_length() {
     using list1 = Creator<int, float, double, int, char >::type;
     using list2 = Creator<int>::type;
     std::cout << (Length<list1>::value == 5) << std::endl;
     std::cout << (Length<list2>::value == 1) << std::endl;
 }
 
-void remove_by_type() {
+void list_remove_by_type() {
     using list1 = Creator<int, float, double, int, char >::type;
     using list2 = Creator<int, float, double, int, float >::type;
     using list3 = Creator<int, float, double, int, float >::type;
@@ -54,7 +56,7 @@ void remove_by_type() {
     std::cout << std::is_same<RemoveByType<list3, int >::type, Creator<float, double, float >::type>::value << std::endl;
 }
 
-void remove_by_index() {
+void list_remove_by_index() {
     using list = Creator<int, float, double, int, char >::type;
     std::cout << std::is_same<RemoveByIndex<list, 0 >::type, Creator<float, double, int, char >::type>::value << std::endl;
     std::cout << std::is_same<RemoveByIndex<list, 1 >::type, Creator<int, double, int, char >::type>::value << std::endl;
@@ -62,13 +64,13 @@ void remove_by_index() {
     std::cout << std::is_same<RemoveByIndex<list, 10 >::type, list>::value << std::endl;
 }
 
-void join() {
+void list_join() {
     using list1 = Creator<int, float, double, int, char >::type;
     using list2 = Creator<int, float, double, int, char >::type;
     std::cout << std::is_same<Join<list1, list2 >::type, Creator<int, float, double, int, char, int, float, double, int, char>::type>::value << std::endl;
 }
 
-void insert() {
+void list_insert() {
     using list1 = Creator<int, float, double, int, char >::type;
     std::cout << std::is_same<Insert<list1, 0, int >::type, Creator<int, int, float, double, int, char>::type>::value << std::endl;
     std::cout << std::is_same<Insert<list1, 1, char >::type, Creator<int, char, float, double, int, char>::type>::value << std::endl;
@@ -76,14 +78,42 @@ void insert() {
     std::cout << std::is_same<Insert<list1, 10, int >::type, Creator<int, float, double, int, char, int>::type>::value << std::endl;
 }
 
+
+void map_get() {
+    struct Test1;
+    struct Test2;
+    using map1 = map::Creator<
+        Pair<int, float>,
+        Pair<float, char>,
+        Pair<Test1, Test2>,
+        Pair<long, short>>::type;
+    std::cout << std::is_same<map::Get<map1, float >::type, char>::value << std::endl;
+    std::cout << std::is_same<map::Get<map1, double >::type, Empty>::value << std::endl;
+    std::cout << std::is_same<map::Get<map1, Test1 >::type, Test2>::value << std::endl;
+}
+
+void map_set() {
+    struct Test1;
+    struct Test2;
+    using map1 = map::Creator<
+        Pair<Test1, int>>::type;
+    std::cout << std::is_same<map::Get<map1, Test1 >::type, int>::value << std::endl;
+    std::cout << std::is_same<map::Get<map::Set<map1, Test1, Test2>::type, Test1 >::type, Test2>::value << std::endl;
+    std::cout << std::is_same<map::Get<map::Set<map1, int, char>::type, int >::type, char>::value << std::endl;
+}
+
 int main() {
-    replace_by_type();
-    replace_by_index();
-    index_of();
-    find();
-    length();
-    remove_by_type();
-    remove_by_index();
-    join();
-    insert();
+    std::cout << "list test" << std::endl;
+    list_replace_by_type();
+    list_replace_by_index();
+    list_index_of();
+    list_find();
+    list_length();
+    list_remove_by_type();
+    list_remove_by_index();
+    list_join();
+    list_insert();
+    std::cout << "map test" << std::endl;
+    map_get();
+    map_set();
 }
--- type/map.h
+++ type/map.h
@@ -11,20 +11,59 @@
 namespace type {
 namespace map {
 
-template <class ...Args>
-struct Map {
-    typedef list::List<Args...> type;
-};
-
-template <class ...Args>
-struct MapCreator {
-    typedef list::Creator<Args...> type;
-};
+template <class K, class V, class ...Args>
+using Map = list::List<Pair<K, V>, Args...>;
 
 template <class TMap, class K>
-struct Find;
+struct Get;
+
+template <class K>
+struct Get <Empty, K> {
+    typedef Empty type;
+};
+
+template <class Key, class Value, class N>
+struct Get <list::List<Pair<Key, Value>, N>, Key> {
+    typedef Value type;
+};
+
+template <class Key, class Value, class N, class K>
+struct Get <list::List<Pair<Key, Value>, N>, K> {
+    typedef typename Get<N, K>::type type;
+};
+
+template <class TMap, class K, class V>
+struct Set;
+
+template <class K, class V>
+struct Set <Empty, K, V> {
+    typedef typename list::List<Pair<K, V>, Empty> type;
+};
+
+template <class Key, class Value, class N, class V>
+struct Set <list::List<Pair<Key, Value>, N>, Key, V> {
+    typedef typename list::List<Pair<Key, V>, typename Set<N, Key, V>::type> type;
+};
+
+template <class Key, class Value, class N, class K, class V>
+struct Set <list::List<Pair<Key, Value>, N>, K, V> {
+    typedef typename list::List<Pair<Key, Value>, typename Set<N, K, V>::type> type;
+};
 
 
+
+template <class ...Items>
+struct Creator;
+
+template <class K, class V>
+struct Creator<Pair<K, V>> {
+    typedef Map<K, V, Empty> type;
+};
+
+template <class K, class V, class ...Items>
+struct Creator<Pair<K, V>, Items...> {
+    typedef Map<K, V, typename Creator<Items...>::type> type;
+};
 }
 }
 
